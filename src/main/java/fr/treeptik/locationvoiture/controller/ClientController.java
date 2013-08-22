fd2fe7744fc69400e1b8ba25098c0785ac23251d
hassen <khelifi_hassen@yahoo.fr>
2013-08-22 12:36:23 +0200

    commit10

--- src/main/java/fr/treeptik/locationvoiture/controller/ClientController.java
+++ src/main/java/fr/treeptik/locationvoiture/controller/ClientController.java
@@ -1,5 +1,8 @@
 package fr.treeptik.locationvoiture.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import fr.treeptik.locationvoiture.exception.ServiceException;
 import fr.treeptik.locationvoiture.model.Client;
+import fr.treeptik.locationvoiture.model.Voiture;
 import fr.treeptik.locationvoiture.service.ClientService;
 
 @Controller
@@ -16,6 +20,15 @@ public class ClientController {
 	@Autowired
 	private ClientService clientService;
 	
+	
+	@RequestMapping(value = "/client.do", method = RequestMethod.GET)
+	public ModelAndView initForm() throws ServiceException {
+		Client cli = new Client();
+		ModelAndView modelAndView = new ModelAndView("save-client",
+				"client", cli);
+		return modelAndView;
+	}
+
 	@RequestMapping(value="/client.do", method=RequestMethod.POST)
 	public ModelAndView save(Client client) throws ServiceException{
 		
@@ -26,4 +39,14 @@ public class ClientController {
 	}
 	
 
+	@RequestMapping(value="/clients.do", method=RequestMethod.GET)
+	public ModelAndView findAll() throws ServiceException{
+		
+		Map<String, Object> params = new HashMap<String,Object>();
+		params.put("listClients", clientService.findAll());
+		
+		
+		return new ModelAndView("list-client",params);
+		
+	}
 }
