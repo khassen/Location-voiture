a5f14d051d3d22f5079272d44bf5bff0b811ca02
hassen <khelifi_hassen@yahoo.fr>
2013-08-23 16:26:31 +0200

    commit dbase.sql

---
+++ dump.sql
@@ -0,0 +1,111 @@
+-- MySQL dump 10.13  Distrib 5.5.32, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: locationvoiture
+-- ------------------------------------------------------
+-- Server version	5.5.32-0ubuntu0.12.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `prenom` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (8,'patrick@live.com','Michlo','Patrik'),(10,'khelifi_hassen@yahoo.fr','Khelifi','Hassen'),(11,'robi-chala@yahoo.fr','Robi','Chala'),(15,'kich-jean@hotmail.fr','Kichel','Jean');
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reservation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `datePrise` datetime DEFAULT NULL,
+  `dateRetour` datetime DEFAULT NULL,
+  `id_client` int(11) DEFAULT NULL,
+  `id_voiture` int(11) DEFAULT NULL,
+  `dateResev` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_6a75vrb4d1le23asqfal7h4js` (`id_client`),
+  KEY `FK_awscnuvqmcp63ljah2njk13vg` (`id_voiture`),
+  CONSTRAINT `FK_6a75vrb4d1le23asqfal7h4js` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`),
+  CONSTRAINT `FK_awscnuvqmcp63ljah2njk13vg` FOREIGN KEY (`id_voiture`) REFERENCES `voiture` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+INSERT INTO `reservation` VALUES (16,'2013-01-01 00:00:00','2013-02-01 00:00:00',8,1,'2012-12-30 00:00:00'),(17,'2013-01-25 00:00:00','2013-02-20 00:00:00',10,3,'0201-01-05 00:00:00'),(18,'2012-05-01 00:00:00','2013-02-01 00:00:00',11,5,'2012-04-30 00:00:00');
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `voiture`
+--
+
+DROP TABLE IF EXISTS `voiture`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `voiture` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `marque` varchar(255) DEFAULT NULL,
+  `model` varchar(255) DEFAULT NULL,
+  `dateMiseEnCirculation` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `voiture`
+--
+
+LOCK TABLES `voiture` WRITE;
+/*!40000 ALTER TABLE `voiture` DISABLE KEYS */;
+INSERT INTO `voiture` VALUES (1,'BMW','520',NULL),(2,'Audi','A3',NULL),(3,'Toyota','Rav 4',NULL),(5,'Pegeot','207',NULL),(6,'Toyota','RAV 4',NULL),(7,'Renault','Megane',NULL),(9,'Renault','Espace',NULL),(10,'Fiat','500L',NULL),(11,'Toyota','Yaris',NULL);
+/*!40000 ALTER TABLE `voiture` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2013-08-23 16:20:40
